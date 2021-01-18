e75c205556faf5ad0fbddbc282046235dfd0808f
許銘仁 <surprised128@gmail.com>
2021-01-18 21:04:02 +0800

    update 1679. Max Number of K-Sum Pairs

--- 1679. Max Number of K-Sum Pairs.java
+++ 1679. Max Number of K-Sum Pairs.java
@@ -1,37 +1,35 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        /* 紀錄使用過的數字 */
+        /* 由小排至大，故只需要檢查陣列前半段即可 */
+        Arrays.sort(nums);
+        
+        /* 收集不重複之數字 */
         ArrayList<Integer> record = new ArrayList<>();
         
         /* 統計所有數字與其數量 */
         HashMap<Integer, Integer> list = new HashMap<>();
         for(int num : nums) {
             if(!list.containsKey(num)) {
+                record.add(num);
                 list.put(num, 1);
             } else {
                 list.put(num, list.get(num)+1);
             }
         }
         
-        /* 由小排至大，故只需要檢查陣列前半段即可 */
-        Arrays.sort(nums);
-        
         int count = 0;
-        for(int i = 0;i<nums.length/2;i++) {
-            if(!record.contains(nums[i])) {
-                
-                /* 判斷是否有相加剛好等於K的相對應數存在 */
-                if(list.containsKey(k-nums[i])) {
-                    /* 避免數字相同 */
-                    if(nums[i] == (k-nums[i])) {
-                        count += list.get(nums[i])/2;
-                    } else {
-                        count += Math.min(list.get(nums[i]), list.get(k-nums[i]));
-                    }
+        int len = record.size()%2 == 0 ? record.size()/2 : (record.size()/2)+1;
+        for(int i = 0;i<len;i++) {
+            /* 判斷是否有相加剛好等於K的相對應數存在 */
+            if(list.containsKey(k-record.get(i))) {
+                /* 避免數字相同 */
+                if(record.get(i) == (k-record.get(i))) {
+                    count += list.get(record.get(i))/2;
+                } else {
+                    count += Math.min(list.get(record.get(i)), list.get(k-record.get(i)));
                 }
-                
-                record.add(nums[i]);
-                record.add(k-nums[i]);
+                /* 數字遞增，相對應的數會遞減，故可以將搜索範圍變小 */
+                len = record.indexOf(k-record.get(i));
             }
         }
         return count;
